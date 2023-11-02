65ed5ad373d7b3f3df7b34481b258380e5a2fa28
Chad Yu <chadyu@dhcp-vl2041-3104.eduroam.cornell.edu>
2023-11-02 15:02:55 -0400

    feat: very basic gp posterior compute implemented, along with a plotter to visualize the covariance and predictions

--- mygp/gp.py
+++ mygp/gp.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class GP:
-    def __init__(self, train_x, train_y, kernel, sigma_n=1e-8):
+    def __init__(self, X_s, train_x, train_y, kernel, sigma_n=1e-8):
         """
-        Initialize Gaussian Process with training data and kernel function, and set prior distribution.
+        Initialize Gaussian Process with training data and kernel function, and set prior distribution on train data.
 
         Args:
             train_x: (m x d) matrix of m vectors in R^d
@@ -16,10 +17,14 @@ class GP:
         self.Y = train_y
         self.kernel = kernel
 
-        self.mu = np.zeros(len(train_x))
-        self.cov = kernel(train_x, train_x) + sigma_n**2 * np.eye(len(train_x))
+        self.X_s = X_s
+        self.sigma_n = sigma_n
 
-    def predict(self, test_x):
+        # Prior mean and covariance on test points
+        self.mu = np.zeros(len(X_s))
+        self.cov = kernel(X_s, X_s) + sigma_n**2 * np.eye(len(X_s))
+
+    def predict(self, X_s):
         """
         Computes posterior mean and covariance matrix for given test data, as well as the log marginal likelihood.
 
@@ -30,14 +35,46 @@ class GP:
             (n x 1) vector of predicted means
             (n x n) matrix of predicted covariances
         """
-        L_cov = np.linalg.cholesky(self.cov)
+        # Lower triangular solve to obtain alpha and thus posterior mean
+        L_cov = np.linalg.cholesky(
+            self.kernel(self.X, self.X) + self.sigma_n**2 * np.eye(len(self.X))
+        )
         alpha = np.linalg.solve(L_cov.T, np.linalg.solve(L_cov, self.Y))
-        K_s = self.kernel(self.X, test_x)
-        K_ss = self.kernel(test_x, test_x)
+        K_s = self.kernel(self.X, X_s)
         mu_s = K_s.T @ alpha
+        self.mu = mu_s.reshape(
+            -1,
+        )
 
+        # Compute posterior covariance matrix
+        K_ss = self.cov
         v = np.linalg.solve(L_cov, K_s)
         cov_s = K_ss - v.T @ v
+        self.cov = cov_s
 
-        log_likelihood = -0.5 * self.Y.T @ alpha - np.sum(np.log(np.diag(L_cov))) - len(self.X) / 2 * np.log(2 * np.pi)
+        # Compute log marginal likelihood
+        log_likelihood = (
+            -0.5 * self.Y.T @ alpha
+            - np.sum(np.log(np.diag(L_cov)))
+            - len(self.X) / 2 * np.log(2 * np.pi)
+        )
         return mu_s, cov_s, log_likelihood
+
+    def plot(self, train_x=None, train_y=None, samples=[]):
+        confidence_interval = 1.96 * np.sqrt(np.diag(self.cov))
+
+        plt.fill_between(
+            self.X_s.flatten(),
+            self.mu + confidence_interval,
+            self.mu - confidence_interval,
+            alpha=0.1,
+            color="b",
+        )
+
+        plt.plot(self.X_s, self.mu, "r", lw=1, label="Mean")
+        for i in range(len(samples)):
+            plt.plot(self.X_s, samples[i], lw=1, label=f"Sample {i+1}")
+        if train_x is not None and train_y is not None:
+            plt.plot(train_x, train_y, "ro", label="Training points")
+        plt.legend()
+        plt.show()
--- mygp/kernel.py
+++ mygp/kernel.py
@@ -14,5 +14,42 @@ def rbf(X, Y, l=1.0, sigma_f=1.0):
     Returns:
         (m x n) matrix of RBF kernel values
     """
-    distXY = np.sum(X**2, axis=1).reshape(-1, 1) + np.sum(Y**2, axis=1) - 2 * np.multiply(X, Y.T)
+    distXY = (
+        np.sum(X**2, axis=1).reshape(-1, 1)
+        + np.sum(Y**2, axis=1)
+        - 2 * np.dot(X, Y.T)
+    )
     return sigma_f**2 * np.exp(-0.5 / l**2 * distXY)
+
+
+def linear(X, Y, sigma_b=0.0, sigma_v=1.0, c=0.0):
+    """
+    Returns matrix of linear kernel between every pair of m vectors and n vectors.
+
+    Args:
+        X: (m x d) matrix of m vectors in R^d
+        Y: (n x d) matrix of n vectors in R^d
+        sigma_b: constant variance for linear kernel
+        sigma_v: vertical variance parameter for linear kernel
+        c: constant offset parameter for linear kernel
+    """
+    return sigma_b**2 + sigma_v**2 * (X - c) @ (Y.T - c)
+
+
+def rq(X, Y, l=1.0, sigma=1.0, alpha=1.0):
+    """
+    Returns matrix of rational quadratic kernel between every pair of m vectors and n vectors.
+
+    Args:
+        X: (m x d) matrix of m vectors in R^d
+        Y: (n x d) matrix of n vectors in R^d
+        l: length scale parameter for rational quadratic kernel
+        sigma: signal variance parameter for rational quadratic kernel
+        alpha: alpha parameter for rational quadratic kernel
+    """
+    distXY = (
+        np.sum(X**2, axis=1).reshape(-1, 1)
+        + np.sum(Y**2, axis=1)
+        - 2 * np.multiply(X, Y.T)
+    )
+    return sigma**2 * (1 + distXY / (2 * alpha * l**2)) ** (-alpha)
