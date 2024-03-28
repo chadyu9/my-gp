515b6d67880d494c6a91f91263d35edc908222cd
Chad Yu <cky25@cornell.edu>
2024-03-28 12:22:37 -0400

    feat: TS implemented, added botorch tutorial that implements BO loop with basic logEI

--- mygp/gp.py
+++ mygp/gp.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 import jax.scipy as jsp
+import jax.random as jr
 import matplotlib.pyplot as plt
 import optax
 from flax.training import train_state
@@ -10,7 +11,6 @@ from flax.core import FrozenDict
 class GP:
     def __init__(
         self,
-        X_s,
         train_x,
         train_y,
         kernel,
@@ -22,13 +22,11 @@ class GP:
         Initialize Gaussian Process with training data and kernel function, and set prior distribution on train data.
 
         Args:
-            X_s: (n x d) matrix of n vectors in R^d for test data
             train_x: (m x d) matrix of m vectors in R^d for training data
             train_y: (m x 1) vector of m scalars representing target values
             kernel: kernel function
             sigma_n: assumed noise level of data
         """
-        self.X_s = X_s
         self.X = train_x
         self.Y = train_y
         self.kernel = kernel
@@ -38,9 +36,6 @@ class GP:
             params={**hyperparams, "sigma_n": sigma_n},
             optimizer=optimizer,
         )
-
-        # Prior mean and covariance on test points
-        self.mu, self.cov = self.compute_prior(hyperparams, sigma_n)
 
     def init_train_state(self, params, optimizer):
         """
@@ -54,34 +49,122 @@ class GP:
             training state
         """
         self.train_state = train_state.TrainState.create(
-            apply_fn=self.predict, params=FrozenDict(params), tx=optimizer
+            apply_fn=self.compute_post_dist, params=FrozenDict(params), tx=optimizer
         )
 
-    def compute_prior(self, hyperparams, sigma_n):
+    def rff_basis(self, x, num_basis, key):
+        """
+        Generate a set of Random Fourier feature (RFF) basis functions assuming the kernel is SE.
+
+        Args:
+            x: (d,) vector in R^d
+            num_basis: number of basis functions
+            key: JAX random PRNG key
+
+        Returns:
+            (num_basis,) vector of RFF basis functions
+        """
+
+        # There must be an even number of basis functions for the RFF
+        assert num_basis % 2 == 0
+        (d,) = x.shape
+
+        # Generate random weights
+        weights = jr.multivariate_normal(
+            key, jnp.zeros(d), jnp.eye(d), (num_basis // 2,)
+        )
+
+        # Return RFF basis functions
+        return jnp.sqrt(2 / num_basis) * jnp.concatenate(
+            (
+                jnp.sin(jnp.dot(weights, x)),
+                jnp.cos(jnp.dot(weights, x)),
+            )
+        )
+
+    def prior_f(self, x, num_basis, key):
+        # Split key for computing rff_basis
+        key, subkey = jr.split(key)
+
+        # Compute RFF basis and random weights from standard normal
+        basis = self.rff_basis(x, num_basis, subkey)
+        w = jr.normal(key, (num_basis,))
+        return jnp.dot(w, basis)
+
+    def compute_prior_dist(self, X_s):
         """
         Computes prior mean and covariance matrix for given test data.
 
         Args:
-            hyperparams: hyperparameters of kernel function
-            sigma_n: assumed noise level of data
+            X_s: (n x d) matrix of n vectors in R^d for test data
 
         Returns:
             (n x 1) vector of prior means
             (n x n) matrix of prior covariances
         """
+        # Get hyperparameters and noise level from training state
+        sigma_n = self.train_state.params["sigma_n"]
+        hyperparams = {
+            k: v for k, v in self.train_state.params.items() if k != "sigma_n"
+        }
+
         # Compute prior mean
-        mu_s = jnp.zeros(self.X_s.shape)
+        mu_s = jnp.zeros(X_s.shape)
 
         # Compute prior covariance matrix
-        cov_s = self.kernel(self.X_s, self.X_s, **hyperparams) + sigma_n**2 * jnp.eye(
-            len(self.X_s)
-        )
+        cov_s = self.kernel(X_s, X_s, **hyperparams) + sigma_n**2 * jnp.eye(len(X_s))
 
         return mu_s, cov_s
 
-    def predict(self):
+    def update(self, X_s, y, f, key):
+        """
+        Computes posterior sample given prior sample via pathwise conditioning.
+
+        Args:
+            X_s: (n x d) matrix of n vectors in R^d for test data
+            y: (n x 1) vector of observations with noise at training points
+            f: Random Fourier feature function approximating prior
+            key: JAX random PRNG key
+        """
+        # Get hyperparameters and noise level from training state
+        sigma_n = self.train_state.params["sigma_n"]
+        hyperparams = {
+            k: v for k, v in self.train_state.params.items() if k != "sigma_n"
+        }
+
+        # Lower triangular solve of covariance matrix
+        K = self.kernel(
+            self.X,
+            self.X,
+            **hyperparams,
+        ) + sigma_n**2 * jnp.eye(len(self.X))
+        L_cov = jsp.linalg.cholesky(
+            K,
+            lower=True,
+        )
+
+        # Return prior sample + pathwise update
+        K_s = self.kernel(self.X, X_s, **hyperparams)
+        return jnp.apply_along_axis(
+            f, axis=1, arr=X_s
+        ) + K_s.T @ jsp.linalg.solve_triangular(
+            L_cov.T,
+            jsp.linalg.cho_solve(
+                (L_cov, True),
+                y.ravel()
+                - jnp.apply_along_axis(f, axis=1, arr=self.X)
+                - jr.multivariate_normal(
+                    key, jnp.zeros(len(y)), sigma_n**2 * jnp.eye(len(y))
+                ),
+            ),
+        )
+
+    def compute_post_dist(self, X_s):
         """
         Computes posterior mean and covariance matrix for given test data.
+
+        Args:
+            X_s: (n x d) matrix of n vectors in R^d for test data
 
         Returns:
             (n x 1) vector of predicted means
@@ -92,6 +175,7 @@ class GP:
         hyperparams = {
             k: v for k, v in self.train_state.params.items() if k != "sigma_n"
         }
+
         # Lower triangular solve to obtain alpha and thus posterior mean
         K = self.kernel(
             self.X,
@@ -107,19 +191,16 @@ class GP:
             L_cov.T, jsp.linalg.cho_solve((L_cov, True), self.Y)
         )
 
-        K_s = self.kernel(self.X, self.X_s, **hyperparams)
+        K_s = self.kernel(self.X, X_s, **hyperparams)
         mu_s = K_s.T @ alpha
-        self.mu = mu_s.reshape(
+        mu_s = mu_s.reshape(
             -1,
         )
 
         # Compute posterior covariance matrix
-        K_ss = self.kernel(self.X_s, self.X_s, **hyperparams) + 1e-8 * jnp.eye(
-            len(self.X_s)
-        )
+        K_ss = self.kernel(X_s, X_s, **hyperparams) + 1e-8 * jnp.eye(len(X_s))
         v = jsp.linalg.solve_triangular(L_cov, K_s, lower=True)
         cov_s = K_ss - v.T @ v
-        self.cov = cov_s
 
         return mu_s, cov_s
 
@@ -188,35 +269,35 @@ class GP:
             if epoch % print_every == 0:
                 print(f"Epoch {epoch+1}: loss = {loss:.3f}")
 
-        # Update posterior mean and covariance
-        self.predict()
-
-    def plot(self, train_x=None, train_y=None, samples=[]):
+    def plot(self, X_s, mu, cov, train_x=None, train_y=None, samples=[]):
         """
         Plots mean and error bars for the GP, along with samples.
 
         Args:
+            X_s: (n x d) matrix of n vectors in R^d for test data
+            mu: mean of the distribution to be plotted
+            cov: covariance matrix for the distribution on the test points
             train_x: (m x d) matrix of m vectors in R^d for optional training data
             train_y: (m x 1) vector of m scalars representing target values
             samples: list of (n x 1) vectors of n samples from posterior distribution
         """
 
         # Distance for 95% confidence interval
-        confidence_interval = 1.96 * jnp.sqrt(jnp.diag(self.cov))
+        confidence_interval = 1.96 * jnp.sqrt(jnp.diag(cov))
 
         # Plot error barrs
         plt.fill_between(
-            self.X_s.ravel(),
-            self.mu.ravel() + confidence_interval,
-            self.mu.ravel() - confidence_interval,
+            X_s.ravel(),
+            mu.ravel() + confidence_interval,
+            mu.ravel() - confidence_interval,
             alpha=0.2,
             color="b",
         )
 
         # Plot mean and samples
-        plt.plot(self.X_s.ravel(), self.mu.ravel(), "r", lw=1, label="Mean")
+        plt.plot(X_s.ravel(), mu.ravel(), "r", lw=1, label="Mean")
         for i, sample in enumerate(samples):
-            plt.plot(self.X_s.ravel(), sample, lw=1, label=f"Sample {i+1}")
+            plt.plot(X_s.ravel(), sample, lw=1, label=f"Sample {i+1}")
         if train_x is not None:
             plt.plot(train_x.ravel(), train_y.ravel(), "rx", label="Training points")
         plt.legend()
